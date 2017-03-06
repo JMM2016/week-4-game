7f6b9628c5e249e776dd91f2e4d724fbee4badbb
jmm2016 <sus.magallanes@gmail.com>
2017-03-06 15:02:45 -0600

    game's done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,20 @@
 * if one arg, returns [0-x)
 * if two args, returns [x-y)
 */
-// could add 1 if random != 0 to include the max but enh
 function randomNum(x, y){
 		var random;
 		if (typeof x == 'undefined' && typeof x == 'undefined'){
-			//console.log("it's undef");
 			random = Math.floor(Math.random() * 100);
 		}
-		else if (typeof x != 'undefined' && typeof y == 'undefined'){
-			//console.log("  max: " + x);
+		else if (typeof x != 'undefined' && typeof y == 'undefined'){			
 			random = Math.floor(Math.random() * x);
 		} else {
-			random = Math.floor(Math.random() * (y - x) + x);
-			//console.log("min: " + x + "  max: " + y);
+			random = Math.floor(Math.random() * (y - x) + x);			
 		}							
 		return random;
 }
 	
-$(document).ready(function(){	
-	console.log("yo");	
-	// crystal values between 1 - 12
+$(document).ready(function(){		
 	
 	var game = {
 		win: 0,
@@ -36,9 +30,7 @@ $(document).ready(function(){
 
 		run: function(){
 			this.setValues();		
-			this.updateRecord();
-			//this.pickTotal();						
-			//this.updateRandom();
+			this.updateRecord();						
 		},		
 		
 		updateRecord: function(){
@@ -46,19 +38,8 @@ $(document).ready(function(){
 							  "<p> Losses: " + this.loss + "</p>");
 			$("#match").text(this.random);
 			this.updateScore(); 								
-		},
-
-		// TODO: remove below - most likely
-		/* random num function
-		- that takes an argument which determines the random num returned range??
-		- or just make two functs, one for 1-12 and another for 19-120??
-		*/
-		updateRandom: function() {
-			this.random = randomNum(19,121);
-			$("#match").text(this.random);			
-		},	
+		},				
 		
-		// TODO: don't let them be the same nums
 		assignCrystal: function(){
 			this.c1 = randomNum(1,13);
 			this.c2 = randomNum(1,13);
@@ -68,24 +49,18 @@ $(document).ready(function(){
 
 	 	addCrystalToScore(crystal){
 	 		switch(crystal) {
-				case "c1":
-					console.log("it's c1= " + this.c1);
+				case "c1":					
 					this.score += this.c1;
 					break;
-				case "c2":
-					console.log("it's c2= " + this.c2);
+				case "c2":					
 					this.score += this.c2;
 					break;
-				case "c3":
-					console.log("it's c3= " + this.c3);
+				case "c3":					
 					this.score += this.c3;
 					break;					
-				case "c4":
-					console.log("it's c4= " + this.c4);
+				case "c4":					
 					this.score += this.c4;
-					break;						
-				default:
-					console.log("default");
+					break;										
 			}
 	 		this.updateScore();
 	 		this.isGameOver();
@@ -106,84 +81,29 @@ $(document).ready(function(){
 	 		}
 	 		this.updateRecord();
 	 		this.updateScore();
+	 	},		
+		
+	 	setValues: function(){	 					
+			this.updateRandom();
+			this.assignCrystal();
+	 		this.score = 0;	 		
 	 	},
 
-	 	setValues: function(){
-	 		this.random = randomNum(19,121);
-	 		this.assignCrystal();
-	 		this.score = 0;
-	 		/*	 		
-	 		//this.random = randomNum(19,121);
-			this.assignCrystal();
-			this.score = 0;
-			*/
-	 	}
-		// assign to crystals function ? well, won't need if have separate rand functs				
+		updateRandom(){
+			var temp = this.random;
+			this.random = randomNum(19,121);
+			while(temp == this.random){
+				this.random = randomNum(19,121);
+			}
+		}
+		
 	};
 		
-	$("img").click(function() {
-		//console.log( "Handler for .click() called." );		
-		var crystal = $(this).attr("id");
-		//console.log("crys: " + crys.attr("id") );		// $crys.attr("c1")
-		//console.log(crystal);
-		game.addCrystalToScore(crystal);
-		
-		
-	});
-	
-	/*$("#c1").click(function() {
-		console.log("c1 was pressed");
-	});*/
+	$("img").click(function() {		
+		var crystal = $(this).attr("id");		
+		game.addCrystalToScore(crystal);				
+	});	
 
 	game.run();
 	
 });	
-
-/*function getValue(){
-	$("img").click(function() {
-		console.log( "Handler for .click() called." );
-		
-		var crys = $(this).html();	
-		console.log("crys: " + crys);
-		
-		
-	});		
-}*/
-
-
-/*
-for (var i = 0; i < 15; i++){
-	console.log(randomNum());
-}*/
-/*
-for (var i = 0; i < 15; i++){	
-	console.log("max: 45" + "  " + randomNum(45));
-}
-
-for (var i = 0; i < 15; i++){
-	console.log("min: 5 max: 66" + "  " + randomNum(5,66));
-}
-
-	
-var i = [];
-console.log(typeof i);
-*/
-/**
-* if no arguments returns [0-99]
-* if one arg, returns [0-x)
-* if two args, returns [x-y)
-
-// could add 1 if random != 0 to include the max but enh
-function randomNum(x, y){
-		var random;
-		if (typeof x == 'undefined' && typeof x == 'undefined'){			
-			random = Math.floor(Math.random() * 100);
-		}
-		else if (typeof x != 'undefined' && typeof y == 'undefined'){			
-			random = Math.floor(Math.random() * x);
-		} else {
-			random = Math.floor(Math.random() * (y - x) + x);			
-		}							
-		return random;
-}
-*/
